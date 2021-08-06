57aefc665f29ef64f1a5c2665e6cd9fa534513f4
TechNote <wisjang1@naver.com>
2021-08-06 22:59:23 +0900

    //마이페이지 수정

--- app/src/main/java/com/example/app01/Frag4.java
+++ app/src/main/java/com/example/app01/Frag4.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,12 +12,13 @@ import androidx.fragment.app.Fragment;
 
 public class Frag4 extends Fragment {
     private View view;
+    private Button button1;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag4, container, false);
-
         return view;
+
     }
 }
---
+++ app/src/main/java/com/example/app01/WallyDayActivity.java
@@ -0,0 +1,14 @@
+package com.example.app01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WallyDayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wally_day);
+    }
+}
